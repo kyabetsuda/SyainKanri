34a5e4b0ab093f29ffd8d1743e042ef1e51e5adc
kyabetsuda <junn135246@icloud.com>
2018-04-13 16:54:52 +0900

    first commit

--- src/jp/TsudaJun/SyainKanri/IndexAction.java
+++ src/jp/TsudaJun/SyainKanri/IndexAction.java
@@ -1,29 +1,19 @@
 package jp.TsudaJun.SyainKanri;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
 import jp.TsudaJun.SyainKanri.Dao.CustomDao;
 import jp.TsudaJun.SyainKanri.Model.Syain;
-=======
->>>>>>> origin/master
+
 import lombok.Getter;
 import lombok.Setter;
 
 public class IndexAction {
-	
-<<<<<<< HEAD
 	@Getter @Setter private List<Syain> syains;
 	
 	public String execute() throws Exception{
 		syains = (ArrayList<Syain>)CustomDao.selectAllData();
-=======
-	@Getter @Setter private String hello;
-	
-	public String execute() throws Exception{
-		this.hello = "Hello World!";
->>>>>>> origin/master
 		return "ok";
 	}
 
